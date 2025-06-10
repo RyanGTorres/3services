c3610e5f864d8f9cb62daed98f18866508fe07e8
Fiasco <contato@java10x.dev>
2025-06-10 10:15:15 +0100

    config: Fist RabbitMQ connection on email service, all set

---
+++ email/src/main/java/dev/java10x/email/domain/EmailModel.java
@@ -0,0 +1,32 @@
+package dev.java10x.email.domain;
+
+import dev.java10x.email.enums.EmailStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_EMAIL")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailModel {
+
+    private final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private UUID userId;
+    private String emailFrom;
+    private String emailTo;
+    private String emailSubject;
+    @Column(columnDefinition = "BODY")
+    private String Body;
+    private LocalDateTime sendDateEmail;
+    private EmailStatus statusEmail;
+}
