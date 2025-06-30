0cb0abf723d5ace8285cf69fb92ae16c5a15621f
Fiasco <contato@java10x.dev>
2025-06-30 14:34:30 +0100

    Converter added and working, messages being sended when create a new user

--- user/src/main/java/dev/java10x/user/controller/UserController.java
+++ user/src/main/java/dev/java10x/user/controller/UserController.java
@@ -1,15 +1,19 @@
 package dev.java10x.user.controller;
+
 import dev.java10x.user.domain.UserModel;
 import dev.java10x.user.dto.UserDto;
 import dev.java10x.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Tag(name = "Users", description = "Operations related to users")
 @RestController
 public class UserController {
 
@@ -20,10 +24,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Create a new user", description = "Creates a user and publishes the event for sending email.")
     @PostMapping("/users")
     public ResponseEntity<UserModel> createUser(@RequestBody UserDto userDto) {
         var userModel = new UserModel();
         BeanUtils.copyProperties(userDto, userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveAndPublish(userModel));
     }
+
+    @Operation(summary = "List all users", description = "Returns a list of all registered users.")
+    @GetMapping("list/users")
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        List<UserModel> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    // TODO: Create routes for deletion, editing, and filtering users
+
 }
--- user/src/main/java/dev/java10x/user/service/UserService.java
+++ user/src/main/java/dev/java10x/user/service/UserService.java
@@ -6,6 +6,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -18,6 +20,24 @@ public class UserService {
         this.userProducer = userProducer;
     }
 
+    /**
+     * Retrieves all users from the database.
+     * This method queries the UserRepository to fetch and return a list of all UserModel entities.
+     *
+     * @return a list containing all users in the database
+     */
+    public List<UserModel> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * Saves the given UserModel to the database and then publishes an event with the saved user.
+     * This method is transactional, ensuring that the event is only published if the user is successfully saved.
+     * First, it persists the user using the UserRepository, then it triggers the UserProducer to publish the event.
+     *
+     * @param userModel the user entity to be saved and published
+     * @return the persisted UserModel
+     */
     @Transactional
     public UserModel saveAndPublish (UserModel userModel) {
         userModel = userRepository.save(userModel);
